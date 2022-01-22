ccbc8d8526d25fd38bf4a2006b9cd8294af10436
kiselmen <kiselmen@rambler.ru>
2022-01-22 21:40:37 +0300

    feat: create index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    let str = String(Math.abs(n));
+    let result = '';
+    for (i=0; i<str.length; i++){
+        result = str[i] + result;
+    }
+    return Number(result); 
 }
